1c6e10f9661a16405f668254caf3df15075dfb7a
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-07 02:06:36 +0530

    class2 removed

--- Pradeep1_Maven/src/test/java/Pradeep1_Maven/Class1.java
+++ Pradeep1_Maven/src/test/java/Pradeep1_Maven/Class1.java
@@ -7,16 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Class1 {
-  WebDriver driver;
+ 
 	@Test
   public void test() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Latest drivers servers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.navigate().to("http://www.google.com");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
 		System.out.println("test completed and tested");
-		driver.close();
+		
 		
   }
 }
--- Pradeep1_Maven/src/test/java/Pradeep1_Maven/class2.java
+++
@@ -1,9 +0,0 @@
-package Pradeep1_Maven;
-
-import org.testng.annotations.Test;
-
-public class class2 {
-  @Test
-  public void f() {
-  }
-}
