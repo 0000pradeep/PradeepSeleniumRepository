9765a2313037aefd1e92c0aa73029587dd17b0ea
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-06 23:59:39 +0530

    commit the entire project

---
+++ Pradeep1_Maven/src/test/java/Pradeep1_Maven/Class1.java
@@ -0,0 +1,22 @@
+package Pradeep1_Maven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Class1 {
+  WebDriver driver;
+	@Test
+  public void test() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Latest drivers servers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("http://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		System.out.println("test completed");
+		driver.close();
+		
+  }
+}
---
+++ Pradeep1_Maven/src/test/java/Pradeep1_Maven/class2.java
@@ -0,0 +1,9 @@
+package Pradeep1_Maven;
+
+import org.testng.annotations.Test;
+
+public class class2 {
+  @Test
+  public void f() {
+  }
+}
